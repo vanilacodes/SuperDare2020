bf1e4816cc366466928fba0bb856d85327947fa7
Vanilacodes <61812919+vanilacodes@users.noreply.github.com>
2020-03-29 22:53:27 +0530

    All files uploaded
    First commit to this repository. Quiz app cloned from friendquiz2020.com

---
+++ common/js/helperbea6.js
@@ -0,0 +1,29 @@
+// -------------------- helpers -------------------------------------------------
+
+function get(name){
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + encodeURIComponent(cvalue) + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var cookie = document.cookie;
+    var ca = cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return decodeURIComponent(c.substring(name.length, c.length));
+        }
+    }
+    return "";
+}
